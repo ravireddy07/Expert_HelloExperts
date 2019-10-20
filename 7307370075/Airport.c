3f48ca184ce34ddcdf45a63893cb87f00054927a
ravireddy07 <ravikanthreddy8500@gmail.com>
2019-10-20 11:27:12 +0530

    +Changes Made in Airport.c

--- 7307370075/Airport.c
+++ 7307370075/Airport.c
@@ -3,14 +3,19 @@
 #include<math.h>
 #include "Airport.h"
 
+/*
+####   To do   ####
+1-- Put Printf before taking input from user by telling what type of input has to give....
+2-- 
+*/
 
 int main(int argc, char *argv[]) {
-    Airport airports[MAX];
+    //Airport airports[MAX];
     int choice;
-    string ThreeLetterWord_1, ThreeLetterWord_2;  // Restrict to size 3
-    fillAirports(airports);
+    //string ThreeLetterWord_1, ThreeLetterWord_2;  // Restrict to size 3
+    //fillAirports(airports);
 
-    while(1) {
+    do {
         /* Call the function to print the menu */
         printMenu();
 
@@ -20,10 +25,10 @@ int main(int argc, char *argv[]) {
         switch(choice) {
             case 1:  {
                 /* 1. Get a 3-letter airport code from the user.*/
-                scanf("%s", &ThreeLetterWord_1);
+                //scanf("%s", &ThreeLetterWord_1);
 
                 //2. Call findAirport()
-                findAirport(ThreeLetterWord_1);
+                //findAirport(ThreeLetterWord_1);
 
                 //3. If either the airport's latitude or longitude is -99999, that means the airport was not found. Print an appropriate error.
                 if() {
@@ -40,17 +45,17 @@ int main(int argc, char *argv[]) {
             }
             case 2: {
                 /*                    Call the printAirports() function.*/
-                printAirports();
+                //printAirports();
                 break;
             }
             case 3: {
                 /* 1. Get two 3-letter airport codes from the user. */
-                scanf("%s %s", &ThreeLetterWord_1, &ThreeLetterWord_2);
+                //scanf("%s %s", &ThreeLetterWord_1, &ThreeLetterWord_2);
 
 
                 //2. Call findAirport() twice (one each for each of the airport codes entered).
-                findAirport(ThreeLetterWord_1);
-                findAirport(ThreeLetterWord_2);
+                //findAirport(ThreeLetterWord_1);
+                //findAirport(ThreeLetterWord_2);
 
                 //3. If either airport's latitude or longitude is -99999, that means that airport was not found. Print an appropriate error.
                 if() {
@@ -60,7 +65,7 @@ int main(int argc, char *argv[]) {
 
                 }
                 //4. If both airports found successfully, call the calculateDistance() function.
-                calculateDistance();
+                //calculateDistance();
 
                 //5. Print out the distance between the two airports. */
                 printf("");
@@ -70,10 +75,10 @@ int main(int argc, char *argv[]) {
             }
             case 4: {
                 /* 1. Get a 3-letter airport code from the user.*/
-                scanf("%s", &ThreeLetterWord);
+                //scanf("%s", &ThreeLetterWord);
 
                 //2. Call findAirport() on this origin airport code.
-                findAirport(ThreeLetterWord);
+                //findAirport(ThreeLetterWord);
 
                 //3. If the airport's latitude or longitude is -99999, that means the airport was not found. Print an appropriate error.
                 if() {
@@ -84,10 +89,10 @@ int main(int argc, char *argv[]) {
                 }
 
                 //4. If airport found successfully, get a range in miles (integer) from the user.
-                calculateDistance(); //Print Miles(Integer)
+                //calculateDistance(); //Print Miles(Integer)
 
                 //5. Call findInRange() to get an array of Airports within the specified range of the origin airport.
-                findInRange();
+                //findInRange();
 
                 //6. Print the return array of airports in range. If the result length returned is zero, print a message that no airports were found.*/
                 printf("");
@@ -95,18 +100,18 @@ int main(int argc, char *argv[]) {
                 break;
             }
             case 0: {
-                default: {
-                    printf("Good-bye!\n");
-                    return 0;
-                }
+                printf("Good-bye!\n");
+                break;
             }
         }
-    }
+    } while (choice != 0);
+    
     return 0;
 }
 
 
 void printMenu() {
+    
     printf("\n#########################################\n");
 
     printf("\tAirport Program Menu\n");
