d55cf34b4aba6e947332e7d329cb4729539550dd
ravireddy07 <ravikanthreddy8500@gmail.com>
2019-10-30 13:25:57 +0530

    New Task Solved

--- 7707268875_NumberReverseDigits/SolutionProgramCode.cpp
+++ 7707268875_NumberReverseDigits/SolutionProgramCode.cpp
@@ -2,11 +2,12 @@
 #include<bits/stdc++.h>
 
 void main() {
-  int temp, counter=0, n,ar[100];
+  int temp, counter=0, n,ar[100]; //Array is used to store the single digits of given number
+  
   printf("Enter a Positive Number: ");
   scanf("%d",&n);
   if(n<0) {
-    printf("\nThe given number is not +ve Integer.");
+    printf("\nThe given number is not +ve Integer.");  //Taking Input
     return;
   }
   else {
@@ -14,14 +15,19 @@ void main() {
       temp = num%10;
       ar[counter] = temp;
       num /= 10;
-      counter++;
+      counter++; //counter is used for index and size of the array
     }
   }
-
-  //Printing the Output
-  printf("\nNumbers are : ");
-  for(int j=0;j<counter;j++) {
-    printf("%d ",ar[j]);
-  }
+  
+   //Printing the Output
+   if(counter == 0) {  //counter = 0, means array is empty
+   	return;
+   }
+   else {
+   	printf("\nNumbers are : ");
+    for(int j=0;j<counter;j++) {
+    	printf("%d ",ar[j]);
+    }
+   }
   return;
 }
