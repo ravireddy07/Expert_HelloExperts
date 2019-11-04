028ee801ca3e634f5169afa1e8c72dc24b98f5db
ravireddy07 <ravikanthreddy8500@gmail.com>
2019-11-04 14:36:06 +0530

    Code corrected

--- 7761247275_LetterChecking/Solution.cpp
+++ 7761247275_LetterChecking/Solution.cpp
@@ -14,63 +14,39 @@ if the first and last letters are same, return "apluscompsci"
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX_CHAR 26
-
-//If the first letter is bigger than the last, return "aplus".
-int Case1AndCase2AndCase3(string &str) {
-	int charCount[MAX_CHAR] = {0};      //to keep count of characters, Initially count of all characters is initialized to zero. 
-	int n = str.length();
-	
-	 
-	for (int i=0; i<n; i++) {			//Traverse string and increment count of characters
-		charCount[str[i]-'a']++;
-	}
-	
-	// 'a'-'a' will be 0, 'b'-'a' will be 1, so for location of character in count array we wil do str[i]-'a'. 
-			 
-
-	for(int i=0;i<1;i++) {
-      if(str[i] > str[n]) {
-        return 11;
-      }
-      else if(str[i] < str[n]) {
-        return 21;
-      }
-      else if(str[i] == str[n]) {
-      	return 31;
-	  }
-      else {
-        return 0;
-      }
-    }
-}
 
 
 
-/*
-//Checking if the first and last letters are same.
-int Case3(string s) {
+//Checking conditions.
+int Case1_Case2_Case3(string s) {
 	int n = s.length();
 	if(n < 2) {
-		return -1;
+		return 0;
 	}
-	
-	if(s[0] == s[n - 1]) {
-		return 1;
+	else if(s[0] < s[n-1]) {
+		return 11;                    // first one represent that case1 and second one represent's true.
+	}
+	else if(s[0] > s[n-1]) {
+		return 21;					  // 2 represent that case2 and second one represent's true.
+	}
+	else if(s[0] == s[n - 1]) {
+		return 31;                    // 3 represent that case3 and second one represent's true.
 	}
 	else {
 		return 0;
 	}
 }
-*/
+
 
 
 int main() {
 	string ss;
+	cout<<"Enter String: ";
 	cin>>ss;
 	
 	int result1;
-	result1 = Case1AndCase2AndCase3(ss);
+	result1 = Case1_Case2_Case3(ss);
+	
 	
 	if(result1 == 11) {
 		cout<<endl<<"aplus";
@@ -85,6 +61,7 @@ int main() {
 		return 0;
 	}
 	else{
+		cout<<endl<<"Invalid String..!";
 		return 0;
 	}
 }
