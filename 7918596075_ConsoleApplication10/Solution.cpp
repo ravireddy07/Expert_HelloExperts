7e0678750658093ec8da1d7cae0148cb114158ef
ravireddy07 <ravikanthreddy8500@gmail.com>
2019-11-13 14:42:34 +0530

    code updated

--- 7918596075_ConsoleApplication10/Solution.cpp
+++ 7918596075_ConsoleApplication10/Solution.cpp
@@ -1,79 +1,87 @@
-#include<iostream>
-#include<string>
-
+//#include "stdafx.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-
-struct Grades
+struct StudentInfo
 {
-string name;//Student's name
-int studentID; //Student's Identification Number
-double test [3];
-double average;
-char grade;
-
+	string name;			// student name
+	unsigned int idNum;		// student ID number (Watch for overflow if id num is too long - can make it a string)
+	int *tests;				// pointer to an array of test scores
+	int average;			// average test score
+	char grade;				// course grade
 };
-/* You can declare a array of structs like this :
- Grades variable_name[array_size]; and access it with variable_name[index].member.
- Example Grades details[4]; details[n].name;
- Check your functions and where and when you call them.
-*/
-
-// Function Prototypes.
-int GetGrades(int, int, int);
-int FindAverage(int, int);
 
 
-int main()
-{
-Grades student;
+void compGrade(StudentInfo *, int);
 
-Grades grd;
-//Get Student name
-cout << "Enter the Student name :";
-cin.ignore();
-getline(cin, grd.name);
+int main() {
+	int numScores, numStudents, total;			
 
-//Get Student's ID
-cout << "Enter the Student's Identification Number: ";
-cin >> student.studentID;
+	cout<<"How many test scores per student? ";
+	cin>>numScores;
+	cout<<"How many students? ";
+	cin>>numStudents;
+	
+	StudentInfo *ptrStudents = nullptr;
+	ptrStudents = new StudentInfo[numStudents];
 
-//Get scores
-cout << "Enter three test scores: ";
+	for(int count=0;count<numStudents;count++) {
+		(ptrStudents + count)->tests = new int[numScores];
+		
+		cout<<"Enter the ID number for student "<<(count+1)<<": ";
+		cin>>(ptrStudents + count)->idNum;
+		
+		cout<<"Enter the name for student "<<(count+1)<<": ";
+		cin>>(ptrStudents+count)->name;
+		
+		total=0;
+		
+		for(int count2=0;count2<numScores;count2++) {
+			do {
+				cout<<"For student "<<(count+1)<<" enter score "<<(count2+1)<<": ";
+				cin>>(*(ptrStudents+count)).tests[count2];
+				
+			}while((*(ptrStudents + count)).tests[count2]<0);
+			
+			total += (*(ptrStudents + count)).tests[count2];
+		}
+		
+		(ptrStudents+count)->average = (total/numScores);
+		compGrade(ptrStudents, count);	
+	}
+	
+	cout<<"========================================\n\n";
+	cout<<"\tInfo for all students\n";
+	for(int count=0;count<numStudents;count++) {
+		cout<<"Student "<<(count + 1)<<endl;
+		cout<<"Name: "<<(ptrStudents+count)->name<<endl;
+		cout<<"ID: "<<(ptrStudents+count)->idNum<<endl;
+		cout<<"Average Test Score: "<<(ptrStudents+count)->average<<endl;
+		cout<<"Course Grade: "<<(ptrStudents + count)->grade<<endl;
 
+		cout<<"\n========================================\n";
+	}
 
+	delete[] ptrStudents;
+	ptrStudents = nullptr;
+
+	return 0;
 }
 
-int grade()/* Try making this return a char of letterGrade instead of a int of zero and accept a variable of sum scores*/
-{
-const int numberOfGrades = 3;
 
-int /*numberOfGrades,*/ letterGrade;
-int totalPoints, average;
+void compGrade(StudentInfo *ptrSt, int student) {
+	char grade;
 
-GetGrades(numberOfGrades, totalPoints, average); /* You are trying to call a function thats doesn't exist in the code*/
-
-while (totalPoints >= 0)
-{
-if (totalPoints >= 90 && totalPoints == 100) // 90 and above
-letterGrade = 'A';
-else if (totalPoints >= 80 && totalPoints == 89) //80-89
-letterGrade = 'B';
-else if (totalPoints >= 70 && totalPoints == 79) //70-79
-letterGrade = 'C';
-else if (totalPoints >= 60 && totalPoints == 69) //60-69
-letterGrade = 'D';
-else if (totalPoints >= 0 && totalPoints == 59) //0-59
-letterGrade = 'F';
-
-}
-return 0;
-}
-
-
-int FindAverage(int numericGrade, int numberOfGrades)
-{
-/* Display the average(to what or where)*/
-return (numericGrade) / numberOfGrades;
+	if ((ptrSt + student)->average >= 91 && (ptrSt + student)->average <= 100)
+		(ptrSt + student)->grade = 'A';
+	else if ((ptrSt + student)->average >= 81 && (ptrSt + student)->average <= 90)
+		(ptrSt + student)->grade = 'B';
+	else if ((ptrSt + student)->average >= 71 && (ptrSt + student)->average <= 80)
+		(ptrSt + student)->grade = 'C';
+	else if ((ptrSt + student)->average >= 61 && (ptrSt + student)->average <= 70)
+		(ptrSt + student)->grade = 'D';
+	else if ((ptrSt + student)->average <= 60)
+		(ptrSt + student)->grade = 'F';
 }
